4b3a6dedcce90381c53e13e9972b344d9e7a76d2
Ernest <ernest.okiror@student.moringaschool.com>
2023-03-20 18:18:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1=31
+let num2= 2
+var multiply= (num1 * num2)
+console .log(multiply)
+let random = Math.ceil(Math.random()*10)
+let num3 = 20
+let num4 =16
+let mod =(num3 % num4)
+console. log(mod)
+let nums =[2,4,6,8,10, 20]
+let max = Math.max.apply(null, nums)
